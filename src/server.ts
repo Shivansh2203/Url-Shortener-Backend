eca8e217516097c3ac317f184be23c84e27104a4
Shivansh2203 <168472217+Shivansh2203@users.noreply.github.com>
2025-08-15 14:40:24 +0530

    /route

--- src/server.ts
+++ src/server.ts
@@ -3,23 +3,31 @@ import dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./config/dbConfig";
 import shortUrl from "./routes/shortUrl";
+import { error } from "console";
 
 dotenv.config();
 connectDB();
 
-
-const port = process.env.PORT||5001;
+const port = process.env.PORT || 5001;
 
 const app = express();
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
-app.use(cors({
-    origin :"http://localhost:3000",
-    credentials : true
-}))
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
 
-app.use("/api/",shortUrl);
+app.use("/api/", shortUrl);
+app.get("/", (req, res, next) => {
+  res.send({
+    activeStatus: true,
+    error: false,
+  });
+});
 
-app.listen(port,()=>{
-    console.log(`Server is running on port ${port}`)
-})
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
